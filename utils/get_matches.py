b40ab504cb534ea1050dd18da8b3269e7e242e8f
Your Name <you@example.com>
2026-01-09 01:13:29 +1100

    updating code efficiency

--- utils/get_matches.py
+++ utils/get_matches.py
@@ -29,136 +29,21 @@ class GetMatchesOdds():
         all_matches_ls = []
         
         for _sport in sports:
-            sport_name = _sport['name']
+            # sport_name = _sport['name']
             competitions = _sport['competitions']
             for _competition in competitions:
                 competition_name = _competition['name']
                 if competition_name == 'Racing Offers':
                     continue
                 matches = _competition['matches']
-                for _match in matches:
-                    inPlay = _match['inPlay']
-                    if inPlay is True:
-                        continue
-                    match_name = _match['name']
-                    start_time = _match['startTime']
-                    contestants = _match['contestants']
-                    competitors = _match['competitors'] if 'competitors' in _match else []
-                    betOptionPriority = _match['betOptionPriority']
-
-                    if not any(item in betOptionPriority for item in self.match_result_options):
-                        if not all(item in self.ignore_options for item in betOptionPriority):
-                            print(f"Expected 'Head To Head' in {match_name} betOptionPriority but got {json.dumps(betOptionPriority)}")
-                    if "markets" not in _match.keys():
-                        continue
-                    markets = _match['markets']
-
-                    for _market in markets:
-                        betOption = _market['betOption']
-                        if betOption not in self.match_result_options:
-                            continue
-                        bettingStatus = _market['bettingStatus']
-                        propositions = _market['propositions']
-                        propositions_len = len(propositions)
-                        if propositions_len != 2:
-                            continue
-                        two_dollar_flag = False
-                        for _proposition in propositions:
-                            return_win = _proposition['returnWin']
-                            if two_dollar_flag is False:
-                                two_dollar_flag = True if return_win >= 2.0 and return_win <= self.high_odds else False
-                        if two_dollar_flag is True:
-                            start_time_aest = datetime.fromisoformat(start_time.replace("Z", "+00:00"))
-                            # Convert to AEST
-                            aest_dt = start_time_aest.astimezone(ZoneInfo("Australia/Sydney"))
-                            aest_dt_iso = aest_dt.isoformat()
-                            
-                            match_details = {
-                                "start_time_aest": aest_dt_iso,
-                                "sport_name": sport_name,
-                                "competition_name": competition_name,
-                                "match_name": match_name,
-                                "contestants": contestants,
-                                "propositions": propositions,
-                                "competitors": competitors,
-                                "betOption": betOption,
-                                "bettingStatus": bettingStatus,
-                                "start_time": start_time,
-                                "two_dollar_flag": two_dollar_flag,
-                            }
-                            all_matches_ls.append(match_details)
+                all_matches_ls = self._handle_matches(matches=matches)
         print(f"Total matches with two dollar odds: {len(all_matches_ls)}")
         self.save_file(all_matches_ls=all_matches_ls)
 
     def get_matches_matches(self, input_jn: dict):
 
-        all_matches_ls = []
         matches = input_jn['matches']
-        for _match in matches:
-            inPlay = _match['inPlay']
-            if inPlay is True:
-                continue
-            match_name = _match['name']
-            start_time = _match['startTime']
-            contestants = _match['contestants']
-            competitionName = _match['competitionName']
-            sportName = _match['sportName']
-            competitors = _match['competitors'] if 'competitors' in _match else []
-
-            markets = _match['_links']['markets']
-            payload = {}
-            headers = {
-                "Content-Type": "application/json",
-                "User-Agent": "PostmanRuntime/7.51.0",
-                "Accept": "*/*",
-                "Accept-Encoding": "gzip, deflate, br",
-            }
-            resp_raw = requests.request("GET", markets, headers=headers, data=payload)
-
-            markets_resp_jn = resp_raw.json()
-            
-            betOptionPriority = markets_resp_jn['betOptionPriority']
-            if not any(item in betOptionPriority for item in self.match_result_options):
-                if not all(item in self.ignore_options for item in betOptionPriority):
-                    print(f"Expected 'Head To Head' in {match_name} betOptionPriority but got {json.dumps(betOptionPriority)}")
-            if "markets" not in markets_resp_jn.keys():
-                continue
-            markets = markets_resp_jn['markets']
-
-            for _market in markets:
-                betOption = _market['betOption']
-                if betOption not in self.match_result_options:
-                    continue
-                bettingStatus = _market['bettingStatus']
-                propositions = _market['propositions']
-                propositions_len = len(propositions)
-                if propositions_len != 2:
-                    continue
-                two_dollar_flag = False
-                for _proposition in propositions:
-                    return_win = _proposition['returnWin']
-                    if two_dollar_flag is False:
-                        two_dollar_flag = True if return_win >= 2.0 and return_win <= self.high_odds else False
-                if two_dollar_flag is True:
-                    start_time_aest = datetime.fromisoformat(start_time.replace("Z", "+00:00"))
-                    # Convert to AEST
-                    aest_dt = start_time_aest.astimezone(ZoneInfo("Australia/Sydney"))
-                    aest_dt_iso = aest_dt.isoformat()
-                    
-                    match_details = {
-                        "start_time_aest": aest_dt_iso,
-                        "sport_name": sportName,
-                        "competition_name": competitionName,
-                        "match_name": match_name,
-                        "contestants": contestants,
-                        "propositions": propositions,
-                        "competitors": competitors,
-                        "betOption": betOption,
-                        "bettingStatus": bettingStatus,
-                        "start_time": start_time,
-                        "two_dollar_flag": two_dollar_flag,
-                    }
-                    all_matches_ls.append(match_details)
+        all_matches_ls = self._handle_matches(matches=matches)
         print(f"Total matches with two dollar odds: {len(all_matches_ls)}")
         self.save_file(all_matches_ls=all_matches_ls)
 
@@ -202,3 +87,88 @@ class GetMatchesOdds():
         )
         with open(self.result_path, "w") as f:
             json.dump(adding_matches_ls, f, indent=4)
+
+    def _clean_propositions(self, propositions: list[dict]) -> list[dict]:
+        cleaned_propositions = []
+        keep_keys = [
+            "name",
+            "returnWin",
+            "returnPlace",
+        ]
+        for _proposition in propositions:
+            cleaned_proposition = {}
+            for _key in keep_keys:
+                if _key in _proposition.keys():
+                    cleaned_proposition.update({_key: _proposition[_key]})
+            cleaned_propositions.append(cleaned_proposition)
+        return cleaned_propositions
+
+    def _handle_matches(self, matches: list[dict]) -> list[dict]:
+        all_matches_ls: list[dict] = []
+        for _match in matches:
+            inPlay = _match['inPlay']
+            if inPlay is True:
+                continue
+            match_name = _match['name']
+            start_time = _match['startTime']
+            contestants = _match['contestants']
+            competitionName = _match['competitionName']
+            sportName = _match['sportName']
+            # competitors = _match['competitors'] if 'competitors' in _match else []
+
+            markets = _match['_links']['markets']
+            payload = {}
+            headers = {
+                "Content-Type": "application/json",
+                "User-Agent": "PostmanRuntime/7.51.0",
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br",
+            }
+            resp_raw = requests.request("GET", markets, headers=headers, data=payload)
+
+            markets_resp_jn = resp_raw.json()
+            
+            betOptionPriority = markets_resp_jn['betOptionPriority']
+            if not any(item in betOptionPriority for item in self.match_result_options):
+                if not all(item in self.ignore_options for item in betOptionPriority):
+                    print(f"Expected 'Head To Head' in {match_name} betOptionPriority but got {json.dumps(betOptionPriority)}")
+            if "markets" not in markets_resp_jn.keys():
+                continue
+            markets = markets_resp_jn['markets']
+
+            for _market in markets:
+                betOption = _market['betOption']
+                if betOption not in self.match_result_options:
+                    continue
+                # bettingStatus = _market['bettingStatus']
+                propositions = _market['propositions']
+                propositions_len = len(propositions)
+                if propositions_len != 2:
+                    continue
+                clean_propositions = self._clean_propositions(propositions=propositions)
+                two_dollar_flag = False
+                for _proposition in clean_propositions:
+                    return_win = _proposition['returnWin']
+                    if two_dollar_flag is False:
+                        two_dollar_flag = True if return_win >= 2.0 and return_win <= self.high_odds else False
+                if two_dollar_flag is True:
+                    start_time_aest = datetime.fromisoformat(start_time.replace("Z", "+00:00"))
+                    # Convert to AEST
+                    aest_dt = start_time_aest.astimezone(ZoneInfo("Australia/Sydney"))
+                    aest_dt_iso = aest_dt.isoformat()
+                    
+                    match_details = {
+                        "start_time_aest": aest_dt_iso,
+                        "sport_name": sportName,
+                        "competition_name": competitionName,
+                        "match_name": match_name,
+                        "contestants": contestants,
+                        "propositions": clean_propositions,
+                        # "competitors": competitors,
+                        "betOption": betOption,
+                        # "bettingStatus": bettingStatus,
+                        "start_time": start_time,
+                        # "two_dollar_flag": two_dollar_flag,
+                    }
+                    all_matches_ls.append(match_details)
+        return all_matches_ls
