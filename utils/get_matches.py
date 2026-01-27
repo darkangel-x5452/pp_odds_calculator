4b14d2fa600a1a58b595ce6e981fe26deddd9ff1
Your Name <you@example.com>
2026-01-28 04:04:05 +1100

    adding companies

--- utils/get_matches.py
+++ utils/get_matches.py
@@ -110,7 +110,6 @@ class GetMatchesOdds():
         keep_keys = [
             "name",
             "returnWin",
-            "returnPlace",
         ]
         for _proposition in propositions:
             cleaned_proposition = {}
@@ -195,7 +194,7 @@ class GetMatchesOdds():
                         value = new_copy.pop(key)
                         cleaned_contestants_new.append({key: value, **new_copy})
                 cleaned_contestants = cleaned_contestants_new.copy()
-                proposition_names = [p['name'] for p in clean_propositions]
+                proposition_names = [f"{p['name']}, ({p['returnWin']})" for p in clean_propositions]
                 contestant_full_names = " VERSES ".join(proposition_names)
                 if two_dollar_flag is True:
                     start_time_aest = datetime.fromisoformat(start_time.replace("Z", "+00:00"))
